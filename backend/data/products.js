283e419637c65191ad5baaaf7c966a202faf9e61
Mehmet Emre <mehmetemrep@gmail.com>
2023-08-15 17:43:44 +0300

    added product schema and seeder scripts

---
+++ backend/data/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    name: 'product 1',
+    description: 'description for product 1',
+    code: '123',
+    countInStock: 10,
+  },
+  {
+    name: 'product 2',
+    description: 'description for product 2',
+    code: '123',
+    countInStock: 10,
+  },
+  {
+    name: 'product 3',
+    description: 'description for product 3',
+    code: '123',
+    countInStock: 10,
+  },
+  {
+    name: 'product 4',
+    description: 'description for product 5',
+    code: '123',
+    countInStock: 10,
+  },
+];
+
+export default products;
---
+++ backend/models/productModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
