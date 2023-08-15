519e02d35838be4a4c82f09963ccd2c464a7137f
Mehmet Emre <mehmetemrep@gmail.com>
2023-08-15 17:30:22 +0300

    initial boilplate express.js with userschema

---
+++ backend/data/users.js
@@ -0,0 +1,12 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@email.com',
+    password: bcrypt.hashSync('123456', 10),
+    username: 'admin',
+  },
+];
+
+export default users;
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  authUser,
+  logoutUser,
+  getUserProfile
+} from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.post('/auth', authUser);
+router.post('/logout', logoutUser);
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+
+
+export default router;
